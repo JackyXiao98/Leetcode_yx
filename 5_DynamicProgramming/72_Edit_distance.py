bac1aa6a69f3fd4e7d6a7f3adfe7d678ea9f0c90
Yingtai Xiao <1025965245@qq.com>
2021-07-10 10:15:43 -0400

    Create 72_Edit_distance.py

---
+++ 5_DynamicProgramming/72_Edit_distance.py
@@ -0,0 +1,28 @@
+def minDistance(word1: str, word2: str) -> int:
+    m = len(word1)
+    n = len(word2)
+    # dp table of size (m+1, n+1)
+    dp = [[0] * (n+1) for _ in range(m+1)]
+    for i in range(m+1):
+        dp[i][0] = i
+    for j in range(n+1):
+        dp[0][j] = j
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if word1[i-1] == word2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i-1][j-1]+1, dp[i][j-1]+1, dp[i-1][j]+1)
+    
+    return dp[m][n]
+
+
+if __name__ == '__main__':
+    m = 3
+    n = 2
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    # print(dp)
+    word1 = "horse"
+    word2 = "ros"
+    print(minDistance(word1, word2))
