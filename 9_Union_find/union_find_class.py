4c4e4108c82ef8a1bbd2ffa1652bc4a18590ed3e
Yingtai Xiao <1025965245@qq.com>
2023-07-04 22:13:17 -0700

    Update union_find_class.py

--- 9_Union_find/union_find_class.py
+++ 9_Union_find/union_find_class.py
@@ -29,3 +29,36 @@ class UnionFind():
     def getCount(self):
         return self.count
 
+
+class union_find():
+    def __init__(self, size) -> None:
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+        self.count = size
+    
+    # path compression
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    # union by rank
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            rank_root_x = self.rank[root_x]
+            rank_root_y = self.rank[root_y]
+            if rank_root_x < rank_root_y:
+                self.root[root_x] = root_y
+            elif rank_root_x > rank_root_y:
+                self.root[root_y] = root_x
+            else:
+                self.root[root_y] = root_x
+                self.rank[root_x] = self.rank[root_x] + 1
+            self.count -= 1
+        
+
+
+
